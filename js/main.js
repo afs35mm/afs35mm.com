5fb9a3b337b68f6c8b686d554a7ecd05cd81248c
AS, akqa <afs35mm@gmail.com>
2013-09-13 16:06:36 -0400

    trying new route

--- js/main.js
+++ js/main.js
@@ -3,20 +3,63 @@ var AFS = AFS || {};
 AFS.App = (function(){
 
 	var config = {
-
+		$container : $('#container')
 	};
 
+	var injectAspectRatio = function(){
+		$('.item').prepend('<div class="aspectRatio"></div>');
+	}
 
 	var bindDomEvents = function(){
-		
+		//update columnWidth on window resize
+		$(window).smartresize(function(){
+			console.log(config.$container.width());
+			isotopeInit();	
+			// if(config.$container.width() < 720 ){
+			// 	config.$container.isotope( 'destroy' );
+			// }else{
+			// 	isotopeInit();	
+			// }
+		});
+	};
+
+	var isotopeInit = function(){
+		config.$container.isotope({
+			itemSelector : '.item',
+			masonry: { columnWidth: config.$container.width() / 4 },
+		});
 	};
 
 	var init = function(){
-		var $container = $('#container');
-		$container.masonry({
-			itemSelector: '.item',
-			transitionDuration: 0
+		// initialize Isotope
+		//isotopeInit();
+		//bindDomEvents();
+		
+		var $container = $('#container'),
+		  $body = $('body'),
+		  colW = 60,
+		  columns = null;
+
+		$container.isotope({
+		// disable window resizing
+		resizable: false,
+		masonry: {
+		  columnWidth: colW
+		}
 		});
+
+		$(window).smartresize(function(){
+		// check if columns has changed
+		var currentColumns = Math.floor( ( $body.width() -10 ) / colW );
+		if ( currentColumns !== columns ) {
+		  // set new column count
+		  columns = currentColumns;
+		  // apply width to container manually, then trigger relayout
+		  $container.width( columns * colW )
+		    .isotope('reLayout');
+		}
+
+		}).smartresize(); // trigger resize to set container width
 	};
 
 	return {

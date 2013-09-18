522807cfcc110b5b53d93b2d51bec89bac77cc35
AS, akqa <afs35mm@gmail.com>
2013-09-18 18:10:36 -0400

    adding hover state thing

--- js/main.js
+++ js/main.js
@@ -4,23 +4,46 @@ AFS.App = (function(){
 
 	var config = {
 		container : document.querySelector('#container'),
+		containerWidth: null,
+		hasTouch: Modernizr.touch,
 	};
 
 	var bindDomEvents = function(){
 		$( window ).resize(function() {
+			config.containerWidth = $('#container').width();
 			makePacketry();
 		});
+		$( document ).mousemove(function( event ) {
+			var fillPercentage = (( event.pageX  ) / config.containerWidth );
+			//console.log(fillPercentage);
+			//$('circle').attr('fill-opacity', fillPercentage );
+		});
+		$('.item').on('mouseenter mouseleave', addHoverClass);
+	};
+
+	var addHoverClass = function(){
+		var $this = $(this);
+		if(!config.hasTouch){
+			//$this.toggleClass('hover');	
+			console.log($this.children('bg'));
+			$this.children('.bg').animate({ marginTop: '0px'},80);
+		}else{
+			//$this.children('bg').animate('marginTop', 0);
+		}
+		
 	};
 
 	var makePacketry = function(){
-		console.log('making packets!');
 		var pckry = new Packery( config.container, {
 		  itemSelector: '.item',
 		  isResizeBound : false,
 		});
-		
+
 	};
+
 	var init = function(){
+		config.containerWidth = $('#container').width();
+		$('.item').fadeIn();
 		bindDomEvents();
 		makePacketry();
 	};
@@ -28,7 +51,6 @@ AFS.App = (function(){
 	return {
 		init: init
 	};
-	
 	
 
 })();

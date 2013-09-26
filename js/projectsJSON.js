28937e5e621ed6ece22ce569cb63913ae5b35043
Andrew Schorr <afs35mm@gmail.com>
2013-09-25 23:49:54 -0400

    adding moar projects

--- js/projectsJSON.js
+++ js/projectsJSON.js
@@ -8,7 +8,7 @@ AFS.projects = {
 		roles: [
 			'HTML5', 
 			'CSS3', 
-			'JS', 
+			'JavaScript', 
 		],
 		images: [
 			'https://s3.amazonaws.com/afs35mm/verizon_1.png',
@@ -26,7 +26,7 @@ AFS.projects = {
 		roles: [
 			'HTML', 
 			'SASS', 
-			'JS',
+			'JavaScript',
 			'PHP' 
 		],
 		images: [
@@ -37,5 +37,65 @@ AFS.projects = {
 			'https://s3.amazonaws.com/afs35mm/olympus_5.jpg',
 			'https://s3.amazonaws.com/afs35mm/olympus_6.png',
 		]
-	}
+	},
+	fireworks :{
+		name: 'HTML5 Canvas Fireworks',
+		description: 'HTML5 Canvas Experiment of pixelated fireworks. HTML5 Attributes such as input used to change firework speed, diameter, and size, on a full page Canvas element.',
+		link: 'http://fireworks.afs35mm.com/',
+		roles: [
+			'HTML5',  
+			'JavaScript',
+			'Canvas' 
+		],
+		images: [
+			'https://s3.amazonaws.com/afs35mm/fireworks_1.png',
+			'https://s3.amazonaws.com/afs35mm/fireworks_2.jpg',
+			'https://s3.amazonaws.com/afs35mm/fireworks_3.jpg',
+			'https://s3.amazonaws.com/afs35mm/fireworks_4.jpg',
+			'https://s3.amazonaws.com/afs35mm/fireworks_5.jpg',
+		]
+	},
+	moodcastr :{
+		name: 'Moodcastr',
+		description: 'Mini webapp that uses both the soundcloud and wunderground weather API to play songs based on your location / weather in the area combination.',
+		link: 'http://moodcastr.com/',
+		roles: [
+			'HTML',  
+			'JavaScript',
+			'APIs' 
+		],
+		images: [
+			'https://s3.amazonaws.com/afs35mm/moodcastr.png',
+		]
+	},
+	ezfade :{
+		name: 'EzFade',
+		description: "Custom jQuery plugin developed for minimalistic, easy slide shows on any sized element. Created for my personal photography site,  <a target='_blank' href='http://andrewschorrphoto.com/'>AndrewSchorrPhoto.com</a>",
+		link: 'http://ezfade.afs35mm.com/',
+		roles: [
+			'JavaScript',
+			'jQuery' 
+		],
+		images: [
+			'https://s3.amazonaws.com/afs35mm/ezfade_1.png',
+			'https://s3.amazonaws.com/afs35mm/ezfade_2.jpg',
+			'https://s3.amazonaws.com/afs35mm/ezfade_3.jpg',
+		]
+	},
+	jeide :{
+		name: 'Jocelyn Eide Copywriter Portfolio',
+		description: "Portfolio site built for friend + copywriter Jocelyn Eide. Served as the designer and developer creating a single page, scrolling site that implements multiple carousels to display previous work.",
+		link: 'http://jocelyneide.com/',
+		roles: [
+			'Design',
+			'UX',
+			'Front End',
+			'Project Manager(?)',
+		],
+		images: [
+			'https://s3.amazonaws.com/afs35mm/jeide_1.png',
+			
+		]
+	},
 };
+
